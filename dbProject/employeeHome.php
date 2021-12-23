742369245fcf08f3f289d84f096ffc592f194565
contactLost <50428210+contactLost@users.noreply.github.com>
2021-12-24 00:39:02 +0300

    employeeHome and guideHome added

---
+++ dbProject/employeeHome.php
@@ -0,0 +1,18 @@
+<?php
+include("./components/navbar.php");
+include("./connection/checkSession.php");
+?>
+
+<html>
+
+Make Hotel Reservation For Customer
+
+Make Tour and Activity Reservation For Customer
+
+Create tours
+
+Assign a guide to tour
+
+
+
+</html>
---
+++ dbProject/guideHome.php
@@ -0,0 +1,1 @@
+<html>Guide Home </html>
