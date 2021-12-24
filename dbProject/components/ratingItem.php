6800192d9fcf25873fc258c6530abf55c57b654e
Eylül Çağlar <caglar.eylul99@gmail.com>
2021-12-24 15:08:04 +0300

    add text field
    Text field for comment page is added

--- dbProject/components/ratingItem.php
+++ dbProject/components/ratingItem.php
@@ -1,13 +1,39 @@
 <!DOCTYPE html>
 <html>
-<body>
-    <div align="center" style="background: #aaa; padding: 50px;">
-        <i class= "fa fa-star fa-2x" style="color: white; border:black"></i>
-        <i class= "fa fa-star fa-2x" style="color: white; border:black"></i>
-        <i class= "fa fa-star fa-2x" style="color: white; border:black"></i>
-        <i class= "fa fa-star fa-2x" style="color: white; border:black"></i>
-        <i class= "fa fa-star fa-2x" style="color: white; border:black"></i>
-    </div>
+
+<head>
+    <style>
+        .submit_button {
+            border-radius: 5px;
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            position: "center";
+            padding: 10px 25px;
+
+        }
+
+        .submit_button:active {
+            transform: translateY(2px);
+        }
+    </style>
+</head>
+
+<body style="text-align:center; margin: 0px;">
+    <form action="./profilePage.php">
+        <textarea name="review" rows="5" cols="80">
+
+  </textarea>
+        <br>
+        <div align="center" style="background: #aaa; padding: 10px;">
+            <i class="fa fa-star fa-2x" style="color: white; border:black"></i>
+            <i class="fa fa-star fa-2x" style="color: white; border:black"></i>
+            <i class="fa fa-star fa-2x" style="color: white; border:black"></i>
+            <i class="fa fa-star fa-2x" style="color: white; border:black"></i>
+            <i class="fa fa-star fa-2x" style="color: white; border:black"></i>
+        </div>
+        <br>
+        <input class="submit_button" type="submit" value="Submit">
+    </form>
+
 </body>
 
 </html>
