6d7f7dc7d287c77802631730b44a1b8dc97cf362
Eylül Çağlar <caglar.eylul99@gmail.com>
2021-12-26 23:36:56 +0300

    add stars to review

--- dbProject/components/ratingItem.php
+++ dbProject/components/ratingItem.php
@@ -24,16 +24,53 @@
   </textarea>
         <br>
         <div align="center" style="background: #aaa; padding: 10px;">
-            <i class="fa fa-star fa-2x" style="color: white; border:black"></i>
-            <i class="fa fa-star fa-2x" style="color: white; border:black"></i>
-            <i class="fa fa-star fa-2x" style="color: white; border:black"></i>
-            <i class="fa fa-star fa-2x" style="color: white; border:black"></i>
-            <i class="fa fa-star fa-2x" style="color: white; border:black"></i>
+            <i class="fa fa-star fa-2x" data-index="0"></i>
+            <i class="fa fa-star fa-2x" data-index="1"></i>
+            <i class="fa fa-star fa-2x" data-index="2"></i>
+            <i class="fa fa-star fa-2x" data-index="3"></i>
+            <i class="fa fa-star fa-2x" data-index="4"></i>
         </div>
         <br>
         <input class="submit_button" type="submit" value="Submit">
-    </form>
+    
+    <script src="http://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
+    <script>
+        var rate = -1;
+        var currentStar;
+        $(document).ready(function () {
+            whiteColoredStar();
+            $('.fa-star').on('click', function(){
+                rate = parseInt($(this).data('index'));
+            });
+            $('.fa-star').mouseover(function(){
+                whiteColoredStar();
 
+                currentStar = parseInt($(this).data('index'));
+
+                for(var i = 0; i <= currentStar; i++)
+                    $('.fa-star:eq('+i+')').css('color', 'yellow');
+                
+            });
+            $('.fa-star').mouseleave(function(){
+                whiteColoredStar();
+
+                if(rate != -1){
+                    for(var i = 0; i <= rate; i++)
+                        $('.fa-star:eq('+i+')').css('color', 'yellow');
+
+                }
+            });
+
+            
+        });
+
+        function whiteColoredStar(){
+            $('.fa-star').css('color', 'white');
+        }
+
+        
+    </script>
+    </form>
 </body>
 
 </html>
