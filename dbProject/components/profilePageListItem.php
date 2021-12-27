f2f9bfc23a32ce44c5243e0101bd81e2cea1545b
Eylül Çağlar <caglar.eylul99@gmail.com>
2021-12-27 21:46:00 +0300

    insert photos and change tables

--- dbProject/components/profilePageListItem.php
+++ dbProject/components/profilePageListItem.php
@@ -1,33 +1,65 @@
+<?php
+$user_id = $_SESSION['user_id'];
+$query = "select fname, email, phone_num from account where user_id = " . $user_id . "";
+?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
 
-<style>
-* {
-    box-sizing: border-box;
-}
-.user_detail_row {
-    display: table;
-    width: 100%;
-    /*height: 200px;*/
-}
-.col {
-    float: left;
-    width: 50%;
-}
+    <style>
+        * {
+            box-sizing: border-box;
+        }
 
-.row:after {
-    display: table;
-}
-</style>
+        .user_detail_row {
+            display: table;
+            width: 100%;
+            /* height: 200px;*/
+            border: solid;
+            border-color: black;
+        }
+
+        .col {
+            float: left;
+            width: 50%;
+        }
+
+        .user_details {
+            position: left;
+        }
+
+        .row:after {
+            display: table;
+        }
+    </style>
 </head>
 
-    
+
 
 <body>
-    <div class="user_detail_row" style="background-color:red">
-        <img src="./img/user_icon.png" style="width:50px;height:60px;">
-        <h2>User Details</h2>
+    <div class="user_detail_row">
+        <div class="user_details">
+            <?php
+            while ($information = $result->fetch_array(MYSQLI_NUM)) {
+            ?>
+            <div class="user_info">
+                <h4>
+                    <?php echo "Name: " + $information[0] ?>
+                </h4>
+                <h4>
+                    <?php echo "e-mail: " + $information[1] ?>
+                </h4>
+                <h4>
+                    <?php echo "Phone: " + $information[2] ?>
+                </h4>
+            </div>
+
+            <?php
+            }
+            ?>
+        </div>
     </div>
     <div class="row">
         <div class="col" style="background-color:#aaa;">
