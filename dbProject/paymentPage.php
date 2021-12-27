638da25886e0bc5cbcca70bb24d7f2888cad6e96
KeremAlemdar <kerem.alemdar.54.60@hotmail.com>
2021-12-28 00:03:42 +0300

    Update paymentPage.php

--- dbProject/paymentPage.php
+++ dbProject/paymentPage.php
@@ -28,92 +28,50 @@ if ($tours->num_rows == 0) {
 <head>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <style>
-        .all {
-            width: 70%;
-            display: flex;
-            justify-content: center;
+        .fullActivity {
+            width: 80%;
         }
 
-        .tours {
-            display: flex;
-            justify-content: center;
-            flex-wrap: wrap;
+        .tour .information {
+            border: 3px solid red;
         }
 
-        .tours>* {
-            flex: 40%;
-            /* increase number of items in a row */
-        }
-
-        .tour {
-            border: 2px solid black;
-            display: flex;
-            width: 40%;
+        .tour img {
+            width: 100%;
+            border: 3px solid blue;
             height: auto;
-            /* 40% dı, 2 liyken kücülmeyi çözmek için auto yaptım*/
         }
 
-        .information {
-            margin-left: 2.5%;
-            margin-right: 2.5%;
-            width: 45%;
-            height: 100%;
-            display: flex;
-            flex-direction: column;
-        }
-
-        img {
-            width: 100%;
-            height: 100%;
-        }
-
-        .img {
-            width: 50%;
-            height: 100%;
-        }
-
-        .decline {
-            width: 60%;
-            display: flex;
-            justify-content: space-between;
-
-        }
-
-        .delete {
-            width: 20%;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-        .empty {
-            font-size: 40px;
-            display: flex;
-            justify-content: center;
-            margin-top: 10%;
-        }
-
-        .form {
-            display: flex;
-        }
-
-        .activity {
-            width: 10%;
-            /* increase activity size */
-            justify-content: center;
-            margin-left: 2.5%;
-            margin-right: 2.5%;
-        }
-
-        .information {
-            display: flex;
-            justify-content: space-between;
-            width: 100%;
+        .tour .imagepart {
+            width: 65%;
+            height: auto;
         }
 
         .activity img {
-            width: 100%;
-            height: 100%;
+            width: 20%;
+            height: auto;
+        }
+
+        .tour {
+            display: flex;
+            border: 3px solid black;
+        }
+
+        .activities {
+            margin-left: 5%;
+        }
+
+        .activity_form {
+            display: flex;
+        }
+
+        .activity_form .button {
+            display: flex;
+            align-items: center;
+        }
+
+        .activity_link_button {
+            display: flex;
         }
     </style>
 </head>
@@ -130,59 +88,68 @@ if ($tours->num_rows == 0) {
     <?php
     while ($tuple = $tours->fetch_array(MYSQLI_NUM)) {
     ?>
-        <form class="form" action='deleteTourFromBucket.php.php' method="post">
-            <div>
-                <input class="input" type="submit" value="Remove">
-            </div>
-            <div>
-                <input type="hidden" id="tour_id" name="tour_id" value=<?php echo $tuple[0] ?>>
-            </div>
-            <div class="tour">
-                <div class="img">
-                    <a href='./tourDetails.php?id=<?php echo $tuple[0] ?>'>
-                        <img src='./img/<?php echo $tuple[4] ?>' />
-                    </a>
-                </div>
-                <div class="information">
-                    <h1>
-                        <?php echo $tuple[5] ?>
-                    </h1>
-                    <h1>
-                        <?php echo $tuple[3] ?>
-                    </h1>
-                </div>
-            </div>
-            <?php
-            $query = "SELECT `A`.* FROM `activity` AS `A` , `tour` AS `T` ,`tour_bucket` AS `TB` 
-WHERE `TB`.`tour_id` = `T`.`tour_id` AND `TB`.`activity_id` = `A`.`activity_id` AND user_id = 1";
-            $activities = $mysqli->query($query);
-            while ($tuple = $activities->fetch_array(MYSQLI_NUM)) {
-            ?>
-                <form class="form" action='deleteActivityFromBucket.php.php' method="post">
-                    <div class="activities">
-                        <div class='activity'>
-                            <div>
-                                <img src='./img/<?php echo $tuple[6] ?>' />
-                            </div>
-                            <div class='information'>
-                                <div>
-                                    <?php echo $tuple[2] ?>
-                                </div>
-                                <div>
-                                    <?php echo $tuple[4] ?>
-                                </div>
-                            </div>
+        <div class="fullActivity">
+            <form class="form" action='deleteTourFromBucket.php.php' method="post">
+                <div class="activity_form">
+
+                    <div>
+                        <input type="hidden" id="tour_id" name="tour_id" value=<?php echo $tuple[0] ?>>
+                    </div>
+                    <div class="tour">
+                        <div class="imagepart">
+                            <a href='./tourDetails.php?id=<?php echo $tuple[0] ?>'>
+                                <img src='./img/<?php echo $tuple[4] ?>' />
+                            </a>
+                        </div>
+                        <div class="information">
+                            <h1>
+                                <?php echo $tuple[5] ?>
+                            </h1>
+                            <h1>
+                                <?php echo $tuple[3] ?>
+                            </h1>
                         </div>
                     </div>
-                    <div>
+                    <div class="button">
                         <input class="input" type="submit" value="Remove">
                     </div>
+                </div>
+                <?php
+                $query = "SELECT `A`.* FROM `activity` AS `A` , `tour` AS `T` ,`tour_bucket` AS `TB` 
+WHERE `TB`.`tour_id` = `T`.`tour_id` AND `TB`.`activity_id` = `A`.`activity_id` AND user_id = 1";
+                $activities = $mysqli->query($query);
 
-                </form>
-            <?php
-            }
-            ?>
-        </form>
+                while ($tuple = $activities->fetch_array(MYSQLI_NUM)) {
+                ?>
+                    <div class="activities">
+                        <form class="form" action='deleteActivityFromBucket.php.php' method="post">
+                            <div class="activity_link_button">
+                                <div class='activity'>
+                                    <div>
+                                        <img src='./img/<?php echo $tuple[6] ?>' />
+                                    </div>
+                                    <div class='information'>
+                                        <div>
+                                            <?php echo $tuple[2] ?>
+                                        </div>
+                                        <div>
+                                            <?php echo $tuple[4] ?>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="activity_button">
+                                    <input class="input" type="submit" value="Remove">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                <?php
+                }
+                ?>
+
+            </form>
+        </div>
     <?php
     }
     ?>
