b5dfddf29e2877dfcda4ca3ee478f9ea17b88c87
Eylül Çağlar <caglar.eylul99@gmail.com>
2021-12-27 23:43:17 +0300

    update Profile Page
    Hotel Reservations are added

--- dbProject/components/profilePageListItem.php
+++ dbProject/components/profilePageListItem.php
@@ -1,7 +1,17 @@
 <?php
-$user_id = $_SESSION['user_id'];
-$query = "select fname, email, phone_num from account where user_id = " . $user_id . "";
+include("./connection/checkSession.php");
+//$user_id = $_SESSION['user_id'];
+$query = "SELECT fname, email, phone_num FROM account WHERE user_id = " . 1 . "";
+$result = $mysqli->query($query);
+$query = "SELECT hotel_id FROM Reservation NATURAL JOIN reservation_hotelR NATURAL JOIN reserve WHERE user_id = " . 1 ."";
+$hotel_id_result = $mysqli->query($query);
+$hotel_id = $hotel_id_result->fetch_array(MYSQLI_NUM);
+$query = "SELECT * from Hotel WHERE hotel_id = " . $hotel_id[0] ."";
+$hotel_info = $mysqli->query($query);
 ?>
+
+
+
 
 <!DOCTYPE html>
 <html>
@@ -42,31 +52,56 @@ $query = "select fname, email, phone_num from account where user_id = " . $user_
     <div class="user_detail_row">
         <div class="user_details">
             <?php
-            while ($information = $result->fetch_array(MYSQLI_NUM)) {
+            $user_info = $result->fetch_array(MYSQLI_NUM);
             ?>
             <div class="user_info">
                 <h4>
-                    <?php echo "Name: " + $information[0] ?>
+                    <?php echo "Name: ", $user_info[0] ?>
                 </h4>
                 <h4>
-                    <?php echo "e-mail: " + $information[1] ?>
+                    <?php echo "e-mail: ", $user_info[1] ?>
                 </h4>
                 <h4>
-                    <?php echo "Phone: " + $information[2] ?>
+                    <?php echo "Phone: ", $user_info[2] ?>
                 </h4>
             </div>
 
             <?php
-            }
+            
             ?>
         </div>
     </div>
     <div class="row">
         <div class="col" style="background-color:#aaa;">
             <h2>Hotel Reservations</h2>
-            <p>Selectable rows</p>
+
+            <?php
+            while ($tuple = $hotel_info->fetch_array(MYSQLI_NUM)) {
+            # Tour(tour_id, start_date, end_date, tour_information, image)
+            ?>
+                <div class="hotel">
+                    <div class="img">
+                        <a href='./hotelDisplay.php?id=<?php echo $tuple[0] ?>'>
+                            <img src='./img/<?php echo $tuple[5] ?>' />
+                        </a>
+                    </div>
+                    <div class="information">
+                        <h2>
+                            <?php echo $tuple[1], "  ",  $tuple[2]?>
+                        </h2>
+                        <h3>
+                            <?php echo $tuple[3], " STAR HOTEL "?>
+                        </h3>
+
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
+            
         </div>
         <div class="col" style="background-color:#bbb;">
+        
             <h2>Tour Reservations</h2>
             <p>Selectable rows</p>
         </div>
@@ -74,3 +109,31 @@ $query = "select fname, email, phone_num from account where user_id = " . $user_
 </body>
 
 </html>
+
+
+ <!-- <div class="all">
+        <div class="tours">
+            <?php
+            while ($tuple = $result->fetch_array(MYSQLI_NUM)) {
+            # Tour(tour_id, start_date, end_date, tour_information, image)
+            ?>
+                <div class="tour">
+                    <div class="img">
+                        <a href='./tourDetails.php?id=<?php echo $tuple[0] ?>'>
+                            <img src='./img/<?php echo $tuple[4] ?>' />
+                        </a>
+                    </div>
+                    <div class="information">
+                        <h1>
+                            <?php echo $tuple[5] ?>
+                        </h1>
+                        <h1>
+                            <?php echo $tuple[3] ?>
+                        </h1>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
+    </div>  -->
