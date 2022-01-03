9302d8b7a5fc161d25bc54a72b7af9f912eac5b2
ismetalp98 <alp.eren@ug.bilkent.edu.tr>
2022-01-03 17:20:17 +0300

    navbar fixes

--- dbProject/components/navbarFlight.php
+++ dbProject/components/navbarFlight.php
@@ -1,66 +1,55 @@
 <!DOCTYPE html>
-<html>
-<head>
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <style>
-        .topnav {
-            display: flex;
-            justify-content: center;
-            font-size: 35px;
-            margin-bottom: 2.5%;
-        }
-        .search {
-            display: flex;
-            justify-content: center;
-            margin-bottom: 3.5%;
-        }
-        .search input{
-            border-radius: 3px;
-            border-color: #cc9900;
-            height: 150%;
-            font-size: 20px;
-        }
-        .search button{
-            width: 8%;
-            font-size: 25px;
-            border-radius: 3px;
-            border-color: #cc9900;
-            height: 150%;
-        }
-        button {
-            background-color: white;
-            margin-left: 20px;
-        }
-        .search_bar{
-            display: flex;
-            flex-direction: row;
-        }
-        .search_bar input[type=text]{
-            width: 37%;
-            margin-right: 20px;
-        }
-        .search_bar input[type=date]{
-            width: 25%;
-        }
 
-    </style>
-</head>
-<div>
-    <div class="topnav">
-        <a class="active" href="#home">Home</a>
-        <a href="profilePage.php">Profile</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-    </div>
-    <div class="search">
-        <form class="search_bar" action='ticketListPage.php' method='post'>
-            <tr>
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<style>
+    .search {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 3.5%;
+    }
+
+    .search input {
+        border-radius: 3px;
+        border-color: #cc9900;
+        height: 150%;
+        font-size: 20px;
+    }
+
+    .search button {
+        width: 8%;
+        font-size: 25px;
+        border-radius: 3px;
+        border-color: #cc9900;
+        height: 150%;
+    }
+
+    button {
+        background-color: white;
+        margin-left: 20px;
+    }
+
+    .search_bar {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .search_bar input[type=text] {
+        width: 37%;
+        margin-right: 20px;
+    }
+
+    .search_bar input[type=date] {
+        width: 25%;
+    }
+</style>
+
+<div class="search">
+    <form class="search_bar" action='ticketListPage.php' method='post'>
+        <tr>
             <td><input type='text' placeholder="Departure City" name='departureCity'></td>
             <td><input type='text' placeholder="Arrival City" name='arrivalCity'></td>
             <td><input type='date' placeholder="Departure Date" name='departureDate'></td>
-            <button type ='submit' name='Search' value='Search' class="fa fa-search"></button>
-            </tr>
-        </form>
-    </div>
+            <button type='submit' name='Search' value='Search' class="fa fa-search"></button>
+        </tr>
+    </form>
 </div>
-</html>
--- dbProject/components/navbarHotel.php
+++ dbProject/components/navbarHotel.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $date = date("Y/m/d");
 $start_date = isset($_GET['start_date']) ? $_GET['start_date'] : $date;
 $end_date =  isset($_GET['end_date']) ? $_GET['end_date'] : $date;
@@ -6,58 +6,43 @@ $searchKey =  isset($_GET['searchKey']) ? $_GET['searchKey'] : "";
 $rate =  isset($_GET['rate']) ? $_GET['rate'] : "";
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <style>
-        .topnav {
-            display: flex;
-            justify-content: center;
-            font-size: 35px;
-            margin-bottom: 2.5%;
-        }
-        .search {
-            display: flex;
-            justify-content: center;
-            margin-bottom: 3.5%;
-        }
-        .search input{
-            border-radius: 3px;
-            border-color: #cc9900;
-            height: 150%;
-            font-size: 20px;
-        }
-        .search button{
-            font-size: 25px;
-            border-radius: 3px;
-            border-color: #cc9900;
-            height: 150%;
-        }
-        button {
-            background-color: white;
-        }
 
-    </style>
-</head>
-<div>
-    <div class="topnav">
-        <a class="active" href="#home">Home</a>
-        <a href="profilePage.php">Profile</a>
-        <a href="#contact">Contact</a>
-        <a href="ticketListPage.php">Ticket</a>
-        <a href="#about">About</a>
-    </div>
-    <div class="search">
-        <form class="search_bar" action='hotelListPage.php' method='get'>
-            <tr>
-            <td><input type='text' placeholder="Search.." name='searchKey' value=<?php echo $searchKey?>></td>
-            <td><input type='number' placeholder="Rate.." name='rate' value=<?php echo $rate?>></td>
-            <td><input type='date' placeholder="Start Date" name='start_date' value=<?php echo $start_date?>></td>
-            <td><input type='date' placeholder="End Date" name='end_date' value=<?php echo $end_date?>></td>
-            <button type ='submit' name='Search' value='Search' class="fa fa-search"></button>
-            </tr>
-        </form>
-    </div>
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<style>
+    .search {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 3.5%;
+    }
+
+    .search input {
+        border-radius: 3px;
+        border-color: #cc9900;
+        height: 150%;
+        font-size: 20px;
+    }
+
+    .search button {
+        font-size: 25px;
+        border-radius: 3px;
+        border-color: #cc9900;
+        height: 150%;
+    }
+
+    button {
+        background-color: white;
+    }
+</style>
+
+
+<div class="search">
+    <form class="search_bar" action='hotelListPage.php' method='get'>
+        <tr>
+            <td><input type='text' placeholder="Search.." name='searchKey' value=<?php echo $searchKey ?>></td>
+            <td><input type='number' placeholder="Rate.." name='rate' value=<?php echo $rate ?>></td>
+            <td><input type='date' placeholder="Start Date" name='start_date' value=<?php echo $start_date ?>></td>
+            <td><input type='date' placeholder="End Date" name='end_date' value=<?php echo $end_date ?>></td>
+            <button type='submit' name='Search' value='Search' class="fa fa-search"></button>
+        </tr>
+    </form>
 </div>
-</html>
