aa5f339bf2805f86d1640bd9770af4ae7f14edf5
KeremAlemdar <kerem.alemdar.54.60@hotmail.com>
2022-01-04 02:31:46 +0300

    update

--- dbProject/connection/checkSession.php
+++ dbProject/connection/checkSession.php
@@ -6,4 +6,12 @@ if (session_status() === PHP_SESSION_NONE) {
 if (!isset($_SESSION['cid'])) {
     // header("location: index.php");
 }
+// include("config.php");
+// if (session_status() === PHP_SESSION_NONE) {
+//     session_start();
+// }
+
+// if (!isset($_SESSION['user_id'])) {
+//     header("location: login.php");
+// }
 ?>
