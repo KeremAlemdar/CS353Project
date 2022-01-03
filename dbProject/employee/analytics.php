b05cdda4147bbb16698efe07948de1ce764cfea7
contactLost <50428210+contactLost@users.noreply.github.com>
2022-01-04 01:10:36 +0300

    Update analytics.php

--- dbProject/employee/analytics.php
+++ dbProject/employee/analytics.php
@@ -11,16 +11,57 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<style>
+    /* Add a black background color to the top navigation */
+    .topnav {
+      background-color: #435d7d;
+      overflow: hidden;
+    }
 
+    /* Style the links inside the navigation bar */
+    .topnav a {
+      float: left;
+      color: #f2f2f2;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+      font-size: 17px;
+    }
+
+    /* Change the color of links on hover */
+    .topnav a:hover {
+      background-color: #ddd;
+      color: black;
+    }
+
+    /* Add a color to the active/current link */
+    .topnav a.active {
+      background-color: #04AA6D;
+      color: white;
+    }
+  </style>
 <link rel="stylesheet" href="./crud.css">
 <?php
 
 include("../connection/checkSession.php");
-include("./employeeNavbar.php");
 ?>
 
 </head>
 <body>
+<div class="topnav">
+    <a class="active" href="./employeeHome.php">BTS TOUR MANAGEMENT SYSTEM</a>
+    <a href="./Tour/tourCrud.php">Tours</a>
+    <a href="./Activity/activityCrud.php">Activites</a>
+    <a href="./TourActivities/tourActivities.php">Pairs</a>
+    <a href="./Hotel/hotelCrud.php">Hotel</a>
+    <a href="./Account/accountCrud.php">Account</a>
+    <a href="./Reservation/MakeReservation.php">Reservation List</a>
+    <a href="./Reservation/reservationAccept.php"> Tour Reservations Acceptance</a>
+    <a href="./Reservation/reservationAcceptHotel.php"> Hotel Reservations Acceptance</a>
+    <a href="../mainPage.php" >Go to customer page</a>
+    <a href="../logout.php">Logout</a>
+
+  </div>
 
 <div class="container-xl">
 	<div class="table-responsive">
